096d928519a8fcbe17dfc089fcb3ae63b9873885
bhavukarora64 <bhavukarora64@gmail.com>
2025-08-28 00:09:52 +0200

    Test not found erron on vercel

--- backend/app.js
+++ backend/app.js
@@ -266,4 +266,5 @@ app.post('/headings', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
